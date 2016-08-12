56656c9476a73b1e849beca58990376649f59134
tedimarkova <tedirama@gmail.com>
2016-08-12 17:00:14 +0300

    ViewHomeIndex

--- PletiTedyPleti/PletiTedyPleti/PletiTedyPleti/Controllers/HomeController.cs
+++ PletiTedyPleti/PletiTedyPleti/PletiTedyPleti/Controllers/HomeController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using PletiTedyPleti.Models;
 
 namespace PletiTedyPleti.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            var posts = db.Posts.OrderByDescending(p => p.Date).Take(3);
+            return View(posts.ToList());
         }
 
         public ActionResult About()
